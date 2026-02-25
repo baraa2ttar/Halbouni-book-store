13b215abf4b227386935b3cf16ac3f2491806478
unknown <baraa2ttar@gmail.com>
2026-02-25 04:09:13 +0300

    create fetch feature books use case

---
+++ lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/book_entity.dart';
+import '../repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo ;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks(){
+    return homeRepo.fetchFeaturedBooks();
+
+  }
+}
