cea2fbc05e65e7145320e1a733bcc1b179925c2f
unknown <baraa2ttar@gmail.com>
2026-02-25 04:51:44 +0300

    refactor use case class

--- lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,15 +1,19 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
 import '../entities/book_entity.dart';
 import '../repos/home_repo.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam >{
   final HomeRepo homeRepo ;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
-  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks(){
-    return homeRepo.fetchFeaturedBooks();
 
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+    // TODO: implement call
+    throw UnimplementedError();
   }
+
 }
